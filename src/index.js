26f70548154aae50bc9381b0dc714a974faf3987
Daniel Strong <danielstrong.ds@gmail.com>
2021-12-26 20:15:52 +0900

    fixed ssr issue

--- src/index.js
+++ src/index.js
@@ -11,25 +11,34 @@ const ackeeTracker = require('ackee-tracker')
  * @param {?Object} options - Ackee options.
  */
 const useAckee = function(pathname, environment, options = {}) {
+	const isServer = typeof window === 'undefined'
+
 	const instance = useMemo(() => {
+		// check if we're on the server
+		if (isServer) return null
+		// if the window is there, start an instance
 		return ackeeTracker.create(environment.server, options)
-	}, [ environment.server, options.detailed, options.ignoreLocalhost, options.ignoreOwnVisits ])
+	}, [ environment.server, options.detailed, options.ignoreLocalhost, options.ignoreOwnVisits, isServer ])
 
 	useEffect(() => {
-		const hasPathname = (
-			pathname != null &&
-			pathname !== ''
-		)
+		// check if there is a valid instance
+		if (!instance) return
 
-		if (hasPathname === false) return
+		// check if pathname is valid
+		if (typeof pathname !== 'string' || !pathname.startsWith('/')) return
 
+		// get the desired attributes and build a url
 		const attributes = ackeeTracker.attributes(options.detailed)
 		const url = new URL(pathname, location)
 
-		return instance.record(environment.domainId, {
+		// create a new record and grab the stop function needed for SPA
+		const { stop } = instance.record(environment.domainId, {
 			...attributes,
 			siteLocation: url.href,
-		}).stop
+		})
+
+		// stop updating the visit duration when a user navigates to a new page
+		return stop
 	}, [ instance, pathname, environment.domainId ])
 }
 
